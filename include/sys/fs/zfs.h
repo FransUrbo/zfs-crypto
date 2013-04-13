3ab63d77121c65ebd5079edbd837e151788eb15b
Turbo Fredriksson <turbo@bayour.com>
2013-04-13 20:01:13 +0200

    This is iSCSI sharing for ZoL (shareiscsi). * Support retrieving active iSCSI sessions:   + Move nfs.c:foreach_nfs_shareopt() to libshare.c:foreach_shareopt()     so that it can be (re)used in smb.c and iscsi.c.   + Add support for options to 'shareiscsi'.     + name/iqn	Full iSCSI Qualified Name (IQN), including identifier 		Default: Generated by iscsi_generate_target() 			 => iqn.YYYY-MM.tld.domain:dataset     + lun	LUN (0-16384) 		Default: 0     + type	Share mode (fileio, blockio, nullio, disk, tape) 		Default: blockio     + iomode	IO mode (wb, wt, ro) 		Default: wt     + blocksize	Logical block size (512, 1024, 2048, 4096) 		Default: Volume blocksize, 4096 if not usable.   + If not called with a name= value, then force setting it.     This so that the IQN doesn't change every month (when     it is regenerated again). * Support for SCST iSCSI implementation. * Retrive active iSCSI sessions * Enum values for iscsi_implementation. * Try to improve performance   + We check for iSCSI availability in the init function only     Limits the number of times we do this - it takes time...   + Use stat() instead of opendir()/closedir().   + Since iscsi_targets is global, we only retreive targets if it's NULL     in iscsi_retrieve_targets(). It should limit the number of times we     fork() and read targets AND sessions.

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -143,6 +143,7 @@ typedef enum {
 	ZFS_PROP_INCONSISTENT,		/* not exposed to the user */
 	ZFS_PROP_SNAPDEV,
 	ZFS_PROP_ACLTYPE,
+	ZFS_PROP_SHAREISCSI,
 	ZFS_NUM_PROPS
 } zfs_prop_t;
 
@@ -315,7 +316,9 @@ typedef enum zfs_share_op {
 	ZFS_SHARE_NFS = 0,
 	ZFS_UNSHARE_NFS = 1,
 	ZFS_SHARE_SMB = 2,
-	ZFS_UNSHARE_SMB = 3
+	ZFS_UNSHARE_SMB = 3,
+	ZFS_SHARE_ISCSI = 4,
+	ZFS_UNSHARE_ISCSI = 5
 } zfs_share_op_t;
 
 typedef enum zfs_smb_acl_op {
